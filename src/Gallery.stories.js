928857a16eb1ed78316a1d26ae377f0808b6fb1a
josefineschaefer <schaeferj@hotmail.de>
2019-10-09 16:26:19 +0200

    storybook for gallery and header

---
+++ src/Gallery.stories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Gallery from './Gallery'
+
+export default {
+  title: 'Gallery'
+}
+
+const entries = [
+  //???
+]
+export const exampleGallery = () => {
+  return <Gallery></Gallery>
+}
+
+// woher kommen die Entries und wie bekomme ich sie in meine Gallery?
